ec778668501e4f42e4e6225df166940c13c1d886
Henrique Gomes <henriquegomesjunqueira@gmail.com>
2021-12-16 22:54:38 -0300

    commit

--- ex007/ex.js
+++ ex007/ex.js
@@ -1,1 +1,8 @@
 console.log("O console funcionou perfeitamente")
+var a=12
+if(a >= 60){
+    console.log("Acima de velocidade permitida")
+}
+else{
+    console.log("Dentro da velocidade da via")
+}
